2622e9444f5f0c5b740e72576dbbb847e22ae868
Rosalia Campusano <rosalia.campusano@ikasle.aeg.eus>
2025-03-27 08:41:03 +0100

    feat: Create Weapon class with the attributes

---
+++ Weapon.mjs
@@ -0,0 +1,8 @@
+export default class Weapon {
+    constructor(name, minLevel, damage, durability) {
+        this.name = name;
+        this.minLevel = minLevel;
+        this.damage = damage;
+        this.durability = durability;
+    }
+}
