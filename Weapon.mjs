f07d6f50baf9c1263f0d5f198ef9e9a445547c8b
Rosalia Campusano <rosalia.campusano@ikasle.aeg.eus>
2025-03-27 13:00:56 +0100

    feat: add getters to show the attributes

--- Weapon.mjs
+++ Weapon.mjs
@@ -5,4 +5,20 @@ export default class Weapon {
         this.damage = damage;
         this.durability = durability;
     }
+
+    getName() {
+        return this.name;
+    }
+    
+    getMinLevel() {
+        return this.minLevel;
+    }
+
+    getDamage() {
+        return this.damage;
+    }
+
+    getDurability() {
+        return this.durability;
+    }
 }
